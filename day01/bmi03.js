89d0e5a48cc35e5348c0f09051241f08bf48d9d0
jjang051tj <jjang051tj@gmail.com>
2024-09-26 13:10:38 +0900

    bmi 구하기

---
+++ day01/bmi03.js
@@ -0,0 +1,18 @@
+function bmi(height, weight) {
+  return weight / ((height / 100) * (height / 100));
+}
+let person01 = {
+  weight: 85,
+  height: 175,
+};
+let person02 = {
+  weight: 95,
+  height: 182,
+};
+
+let result01 = bmi(person01.height, person01.weight);
+let result02 = bmi(person02.height, person02.weight);
+
+document.write(result01);
+document.write("<br>");
+document.write(result02);
