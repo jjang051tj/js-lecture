1fb6439480780ebaf95fe9948b3cb5422fe2852e
jjang051tj <jjang051tj@gmail.com>
2024-10-02 17:26:09 +0900

    lotto scss

--- array/lotto.js
+++ array/lotto.js
@@ -10,28 +10,23 @@ for (let i = 1; i <= 45; i++) {
 //[4~45]  42
 //[5~45]  41
 //[6~45]  40
-const lotto = [];
-for (let j = 0; j < 6; j++) {
-  // 0*45  < Math.random()*45 < 1*45
-  const pickedNum = nums.splice(parseInt(Math.random() * nums.length), 2).pop(); //22
-  console.log(pickedNum);
-  lotto.push(pickedNum);
+for (let k = 0; k < 5; k++) {
+  const lotto = [];
+  const copyNums = [...nums];
+  for (let j = 0; j < 6; j++) {
+    // 0*45  < Math.random()*45 < 1*45
+    const pickedNum = copyNums
+      .splice(parseInt(Math.random() * copyNums.length), 1)
+      .pop(); //22
+    console.log(pickedNum);
+    lotto.push(pickedNum);
+  }
+  lotto.sort(function (a, b) {
+    return a > b ? 1 : -1; //삼항연산자
+  });
+  const lottoList = document.querySelector("#lotto"); //ul
+  for (let i = 0; i < 6; i++) {
+    lottoList.innerHTML += "<li>" + lotto[i] + "</li>";
+  }
+  console.log(lotto);
 }
-console.log(nums);
-console.log(lotto); //[22,33,15,4,2,35];
-lotto.sort(function (a, b) {
-  return a > b ? 1 : -1; //삼항연산자
-  // if (a > b) {
-  //   return 1;
-  // } else if (a < b) {
-  //   return -1;
-  // }
-});
-const lottoList = document.querySelector("#lotto"); //ul
-lottoList.innerHTML += "<li>" + lotto[0] + "</li>";
-lottoList.innerHTML += "<li>" + lotto[1] + "</li>";
-lottoList.innerHTML += "<li>" + lotto[2] + "</li>";
-lottoList.innerHTML += "<li>" + lotto[3] + "</li>";
-lottoList.innerHTML += "<li>" + lotto[4] + "</li>";
-lottoList.innerHTML += "<li>" + lotto[5] + "</li>";
-console.log(lotto);
