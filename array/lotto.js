6584c83b80f0e54923635ef3ce08837f616884b6
jjang051tj <jjang051tj@gmail.com>
2024-10-02 17:36:51 +0900

    lotto color

--- array/lotto.js
+++ array/lotto.js
@@ -4,6 +4,7 @@ for (let i = 1; i <= 45; i++) {
   //nums에 값을 밀어넣기
   nums.push(i);
 }
+const colors = ["yellow", "blue", "red", "gray", "green"];
 //[1~45]  45
 //[2~45]  44
 //[3~45]  43
@@ -26,7 +27,17 @@ for (let k = 0; k < 5; k++) {
   });
   const lottoList = document.querySelector("#lotto"); //ul
   for (let i = 0; i < 6; i++) {
-    lottoList.innerHTML += "<li>" + lotto[i] + "</li>";
+    if (lotto[i] >= 1 && lotto[i] <= 10) {
+      lottoList.innerHTML += `<li style="background-color:orange">${lotto[i]}</li>`;
+    } else if (lotto[i] >= 11 && lotto[i] <= 20) {
+      lottoList.innerHTML += `<li style="background-color:blue">${lotto[i]}</li>`;
+    } else if (lotto[i] >= 21 && lotto[i] <= 30) {
+      lottoList.innerHTML += `<li style="background-color:red">${lotto[i]}</li>`;
+    } else if (lotto[i] >= 31 && lotto[i] <= 40) {
+      lottoList.innerHTML += `<li style="background-color:gray">${lotto[i]}</li>`;
+    } else {
+      lottoList.innerHTML += `<li style="background-color:green">${lotto[i]}</li>`;
+    }
   }
   console.log(lotto);
 }
