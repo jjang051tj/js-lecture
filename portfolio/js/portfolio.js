436bdaa0248a5d79b14568179e8b2e6df841fbbb
jjang051tj <jjang051tj@gmail.com>
2024-10-07 14:52:43 +0900

    iotope js 적용

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,10 +1,11 @@
-fetch("../data/data.json") //서버 주소가 들어가면 됨(db가 반영된 json을 리턴 받을 수 있음)
+fetch("../data/data.json") //서버 주소가 들어가면 됨(db가 반영된 json을 리턴 받을 수)
   .then((response) => response.json())
   .then((json) => {
-    const list = document.querySelector(".list");
     console.log(json.typoList);
+    const list = document.querySelector(".list");
+    //jQuery는 dom(document object model) 을 찾고 조작하는데 최고의 라이브러리이다.
     json.typoList.forEach(function (element, index) {
-      list.innerHTML += `<li>
+      list.innerHTML += `<li class="item">
       <a href="">
         <div class="img">
           <img src="../images/${element.img}" alt="" />
@@ -16,6 +17,13 @@ fetch("../data/data.json") //서버 주소가 들어가면 됨(db가 반영된 j
         </div>
       </a>
     </li>`;
+    });
+    imagesLoaded(".list", function () {
+      const iso = new Isotope(".list", {
+        // options
+        itemSelector: ".item",
+        layoutMode: "masonry",
+      });
     });
   })
   .catch(function (error) {
